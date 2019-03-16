30cf68aabbce36df846e5d500a196dd59be7f84e
hookahpost <hookahpost@gmail.com>
2019-03-16 01:49:57 +0100

    Uploaded Bootstrap.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './assets/logo/HookahPost Temp Logo.png';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <img src={logo} className="App-logo" alt="logo" />
+
+      </div>
+    );
+  }
+}
+
+export default App;
